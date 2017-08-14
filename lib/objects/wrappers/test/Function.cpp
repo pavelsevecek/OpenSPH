1092235eff22f1d4f6f16355392d9ede8a43669a
pavel <sevecek@sirrah.troja.mff.cuni.cz>
2017-08-15 01:38:22 +0200

    added few function tests

---
+++ lib/objects/wrappers/test/Function.cpp
@@ -0,0 +1,26 @@
+#include "objects/wrappers/Function.h"
+#include "catch.hpp"
+#include "utils/Utils.h"
+
+using namespace Sph;
+
+
+TEST_CASE("Function default construct", "[function]") {
+    Function<void(int)> func;
+    REQUIRE_FALSE(func);
+    REQUIRE_ASSERT(func(5));
+}
+
+TEST_CASE("Function functor construct", "[function]") {
+    Function<int(int)> func([](const int i) { return i + 1; });
+
+    REQUIRE(func);
+    REQUIRE(func(5) == 6);
+}
+
+TEST_CASE("Function functor assign", "[function]") {
+    Function<int(int)> func;
+    func = [](const int i) { return i + 1; };
+    REQUIRE(func);
+    REQUIRE(func(5) == 6);
+}
