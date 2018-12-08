a76742d47fdbb2faf7c1e9e8c295246ecff4a291
pavel <sevecek@sirrah.troja.mff.cuni.cz>
2018-12-08 01:13:49 +0100

    fixed compilation

--- gui/windows/PlotView.cpp
+++ gui/windows/PlotView.cpp
@@ -208,7 +208,7 @@ PlotFrame::PlotFrame(wxWindow* parent, const wxSize size, const wxSize padding, 
     sizer->Add(plotView);
     this->SetSizerAndFit(sizer);
 
-    this->Bind(wxEVT_SIZE, [this, toolbarHeight](wxSizeEvent& UNUSED(evt)) {
+    this->Bind(wxEVT_SIZE, [this, toolbarHeight](wxSizeEvent& evt) {
         const wxSize size = evt.GetSize();
         plotView->resize(Pixel(size.x, size.y - toolbarHeight));
     });
