9f5bc76a1759ca72d83293c6e7620e4ceac51bd8
pavel <sevecek@sirrah.troja.mff.cuni.cz>
2018-12-12 23:04:56 +0100

    moved functions to cpp

---
+++ lib/physics/Functions.cpp
@@ -0,0 +1,66 @@
+#include "physics/Functions.h"
+
+NAMESPACE_SPH_BEGIN
+
+Float evalBenzAsphaugScalingLaw(const Float D, const Float rho) {
+    const Float D_cgs = 100._f * D;
+    const Float rho_cgs = 1.e-3_f * rho;
+    //  the scaling law parameters (in CGS units)
+    constexpr Float Q_0 = 9.e7_f;
+    constexpr Float B = 0.5_f;
+    constexpr Float a = -0.36_f;
+    constexpr Float b = 1.36_f;
+
+    const Float Q_cgs = Q_0 * pow(D_cgs / 2._f, a) + B * rho_cgs * pow(D_cgs / 2._f, b);
+    return 1.e-4_f * Q_cgs;
+}
+
+Float getEffectiveImpactArea(const Float R, const Float r, const Float phi) {
+    ASSERT(phi >= 0._f && phi <= PI / 2._f);
+    const Float d = (r + R) * sin(phi);
+    if (d < R - r) {
+        return 1._f;
+    } else {
+        const Float area = sqr(r) * acos((sqr(d) + sqr(r) - sqr(R)) / (2 * d * r)) +
+                           sqr(R) * acos((sqr(d) + sqr(R) - sqr(r)) / (2 * d * R)) -
+                           0.5_f * sqrt((R + r - d) * (d + r - R) * (d - r + R) * (d + r + R));
+        return area / (PI * sqr(r));
+    }
+}
+
+Float getImpactorRadius(const Float R_pb,
+    const Float v_imp,
+    const Float phi,
+    const Float QoverQ_D,
+    const Float rho,
+    const Flags<GetImpactorFlag> flags) {
+    if (flags.has(GetImpactorFlag::EFFECTIVE_ENERGY)) {
+        // The effective impact energy depends on impactor radius, which is what we want to compute; needs
+        // to be solved by iterative algorithm. First, get an estimate of the radius by using the regular
+        // impact energy
+        Float r = getImpactorRadius(R_pb, v_imp, phi, QoverQ_D, rho, EMPTY_FLAGS);
+        if (almostEqual(getEffectiveImpactArea(R_pb, r, phi), 1._f, 1.e-4_f)) {
+            // (almost) whole impactor hits the target, no need to account for effective energy
+            return r;
+        }
+        // Effective energy LOWER than the regular energy, so we only need to increase the impactor, no
+        // need to check for smaller values.
+        Float lastR = LARGE;
+        const Float eps = 1.e-4_f * r;
+        while (abs(r - lastR) > eps) {
+            lastR = r;
+            const Float a = getEffectiveImpactArea(R_pb, r, phi);
+            // effective->regular = divide by a
+            r = getImpactorRadius(R_pb, v_imp, phi, QoverQ_D / a, rho, EMPTY_FLAGS);
+        }
+        return r;
+    } else {
+        // for 'regular' impact energy, simply compute the impactor radius by inverting Q=1/2 m_imp
+        // v^2/M_pb
+        const Float Q_D = evalBenzAsphaugScalingLaw(2._f * R_pb, rho);
+        const Float Q = QoverQ_D * Q_D;
+        return root<3>(2._f * Q / sqr(v_imp)) * R_pb;
+    }
+}
+
+NAMESPACE_SPH_END
--- lib/physics/Functions.h
+++ lib/physics/Functions.h
@@ -90,18 +90,7 @@ INLINE Float computeCriticalFrequency(const Float rho) {
 /// is 50% mass of the parent body, The relation follows the scaling law of Benz & Asphaug (1999).
 ///
 /// \todo replace D with units, do not enforce SI
-INLINE Float evalBenzAsphaugScalingLaw(const Float D, const Float rho) {
-    const Float D_cgs = 100._f * D;
-    const Float rho_cgs = 1.e-3_f * rho;
-    //  the scaling law parameters (in CGS units)
-    constexpr Float Q_0 = 9.e7_f;
-    constexpr Float B = 0.5_f;
-    constexpr Float a = -0.36_f;
-    constexpr Float b = 1.36_f;
-
-    const Float Q_cgs = Q_0 * pow(D_cgs / 2._f, a) + B * rho_cgs * pow(D_cgs / 2._f, b);
-    return 1.e-4_f * Q_cgs;
-}
+Float evalBenzAsphaugScalingLaw(const Float D, const Float rho);
 
 /// \brief Returns the the ratio of the cross-sectional area of the impact and the total area of the
 /// impactor.
@@ -110,21 +99,12 @@ INLINE Float evalBenzAsphaugScalingLaw(const Float D, const Float rho) {
 /// entirely (hit-and-run collision) and does not deliver its kinetic energy into the target. For the same
 /// Q/Q_D, oblique impacts will therefore look 'artificially' weaker than the impacts and lower impacts
 /// angle. Effective impact area can be used to 'correct' the impact energy, so that impacts at high
-/// impact angles are comparable with head-on impats. See \ref Sevecek_2017 for details. \param Q Relative
-/// impact energy (kinetic energy of the impactor divided by the impactor mass) \param R Radius of the
-/// target \param r Radius of the impactor \param phi Impact angle in radians
-INLINE Float getEffectiveImpactArea(const Float R, const Float r, const Float phi) {
-    ASSERT(phi >= 0._f && phi <= PI / 2._f);
-    const Float d = (r + R) * sin(phi);
-    if (d < R - r) {
-        return 1._f;
-    } else {
-        const Float area = sqr(r) * acos((sqr(d) + sqr(r) - sqr(R)) / (2 * d * r)) +
-                           sqr(R) * acos((sqr(d) + sqr(R) - sqr(r)) / (2 * d * R)) -
-                           0.5_f * sqrt((R + r - d) * (d + r - R) * (d - r + R) * (d + r + R));
-        return area / (PI * sqr(r));
-    }
-}
+/// impact angles are comparable with head-on impats. See \ref Sevecek_2017 for details.
+/// \param Q Relative impact energy (kinetic energy of the impactor divided by the impactor mass)
+/// \param R Radius of the target
+/// \param r Radius of the impactor
+/// \param phi Impact angle in radians
+Float getEffectiveImpactArea(const Float R, const Float r, const Float phi);
 
 enum class GetImpactorFlag {
     /// Use effective impact energy instead of 'regular' impact energy when determining impactor size
@@ -139,40 +119,12 @@ enum class GetImpactorFlag {
 /// \param v_imp Impact velocity
 /// \param QoverQ_D Ratio of the impact energy and the critical energy given by the scaling law
 /// \param rho Density of the parent bod
-INLINE Float getImpactorRadius(const Float R_pb,
+Float getImpactorRadius(const Float R_pb,
     const Float v_imp,
     const Float phi,
     const Float QoverQ_D,
     const Float rho,
-    const Flags<GetImpactorFlag> flags) {
-    if (flags.has(GetImpactorFlag::EFFECTIVE_ENERGY)) {
-        // The effective impact energy depends on impactor radius, which is what we want to compute; needs
-        // to be solved by iterative algorithm. First, get an estimate of the radius by using the regular
-        // impact energy
-        Float r = getImpactorRadius(R_pb, v_imp, phi, QoverQ_D, rho, EMPTY_FLAGS);
-        if (almostEqual(getEffectiveImpactArea(R_pb, r, phi), 1._f, 1.e-4_f)) {
-            // (almost) whole impactor hits the target, no need to account for effective energy
-            return r;
-        }
-        // Effective energy LOWER than the regular energy, so we only need to increase the impactor, no
-        // need to check for smaller values.
-        Float lastR = LARGE;
-        const Float eps = 1.e-4_f * r;
-        while (abs(r - lastR) > eps) {
-            lastR = r;
-            const Float a = getEffectiveImpactArea(R_pb, r, phi);
-            // effective->regular = divide by a
-            r = getImpactorRadius(R_pb, v_imp, phi, QoverQ_D / a, rho, EMPTY_FLAGS);
-        }
-        return r;
-    } else {
-        // for 'regular' impact energy, simply compute the impactor radius by inverting Q=1/2 m_imp
-        // v^2/M_pb
-        const Float Q_D = evalBenzAsphaugScalingLaw(2._f * R_pb, rho);
-        const Float Q = QoverQ_D * Q_D;
-        return root<3>(2._f * Q / sqr(v_imp)) * R_pb;
-    }
-}
+    const Flags<GetImpactorFlag> flags);
 
 
 class ImpactCone {
