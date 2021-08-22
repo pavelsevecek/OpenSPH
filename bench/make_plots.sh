633c761fac6038e9e7354666134bfe0890134d8f
pavel <sevecek@sirrah.troja.mff.cuni.cz>
2021-08-22 06:41:27 +0200

    adding artifacts from performance tests

---
+++ bench/make_plots.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+for f in `ls perf-*.csv`
+do
+    sha=${f:5:8}
+    timestamp=`git show -s --format=%ci $sha`
+    echo "Processing SHA $sha (timestamp $timestamp)"
+    cat $f | while read line
+    do
+        name=`echo $line | awk -F',' '{print $1}'`
+        fname="${name// /_}.txt"
+        duration=`echo $line | awk -F',' '{print $4}'`
+        echo "Test $fname took $duration"
+        echo "\"$timestamp\" $duration" >> $fname
+        #echo "$((timestamp+1)) $duration" >> $fname
+        #echo "$((timestamp+2)) $duration" >> $fname
+    done
+done
+
+for f in `ls Finder*.txt`
+do
+   sort -o $f $f
+   plotfile="${f%.*}.pdf"
+   testname="${f//_/ }"
+   testname="${testname%.*}"
+   echo "Plotting $testname to $plotfile"
+   gnuplot -e "\
+     set term pdfcairo size 12,8;\
+     set output '$plotfile';\
+     set bmargin 13;\
+     set title '$testname';\
+     set xtics rotate by -60;\
+     plot '$f' using 2:xticlabels(1) w lp pt 7 not;\
+     set output"
+done
