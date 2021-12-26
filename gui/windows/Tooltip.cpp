074b404a6a9ee429748c97c897146e9746a32e92
pavel <sevecek@sirrah.troja.mff.cuni.cz>
2021-12-26 19:38:41 +0100

    fixed tooltips outside screen area

--- gui/windows/Tooltip.cpp
+++ gui/windows/Tooltip.cpp
@@ -1,4 +1,5 @@
 #include "gui/windows/Tooltip.h"
+#include <wx/display.h>
 #include <wx/sizer.h>
 #include <wx/stattext.h>
 
@@ -6,14 +7,21 @@ NAMESPACE_SPH_BEGIN
 
 Tooltip::Tooltip(wxWindow* parent, wxPoint position, const String& text)
     : wxPopupWindow(parent) {
-    wxPoint screenPosition = parent->ClientToScreen(position) + wxPoint(10, 10);
-    this->SetPosition(screenPosition);
     this->SetSize(wxSize(500, -1));
     wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
     wxStaticText* content = new wxStaticText(this, wxID_ANY, text.toUnicode());
 
     sizer->Add(content, 0, wxEXPAND | wxALL, 8);
     this->SetSizerAndFit(sizer);
+
+    wxPoint screenPosition = parent->ClientToScreen(position) + wxPoint(10, 10);
+    wxDisplay display(wxDisplay::GetFromWindow(parent));
+    wxRect screen = display.GetClientArea();
+    wxSize size = this->GetSize();
+    if (screenPosition.x + size.x > screen.GetWidth()) {
+        screenPosition.x = screen.GetWidth() - size.x;
+    }
+    this->SetPosition(screenPosition);
 }
 
 NAMESPACE_SPH_END
