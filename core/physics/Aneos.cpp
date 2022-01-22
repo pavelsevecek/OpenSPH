288a55398f8fee19c8096006ca7d39b9fbe9f83a
pavel <sevecek@sirrah.troja.mff.cuni.cz>
2022-01-23 00:40:19 +0100

    ANEOS: computing the initial density automatically

--- core/physics/Aneos.cpp
+++ core/physics/Aneos.cpp
@@ -19,20 +19,6 @@ Aneos::TabValue Aneos::TabValue::operator*(const Float f) const {
 
 namespace {
 
-struct TRhoValue {
-    Float u;
-    Float P;
-    Float cs;
-
-    TRhoValue operator+(const TRhoValue& other) const {
-        return { u + other.u, P + other.P, cs + other.cs };
-    }
-
-    TRhoValue operator*(const Float f) const {
-        return { u * f, P * f, cs * f };
-    }
-};
-
 Array<Float> readValuesFromLine(std::ifstream& ifs) {
     std::string line;
     std::getline(ifs, line);
@@ -52,11 +38,10 @@ Array<Float> readValuesFromLine(std::ifstream& ifs) {
 
 } // namespace
 
-//-----------------------------------------------------------------------------------------------------------
-// Aneos implementation
-//-----------------------------------------------------------------------------------------------------------
-
-Lut2D<Aneos::TabValue> Aneos::parseAneosFile(IScheduler& scheduler, const Path& path) {
+Lut2D<EosTabValue> parseAneosFile(const Path& path) {
+    if (path.empty()) {
+        throw InvalidSetup("No ANEOS file specified");
+    }
     std::ifstream ifs(path.native());
     std::string line;
     while (std::getline(ifs, line)) {
@@ -88,11 +73,10 @@ Lut2D<Aneos::TabValue> Aneos::parseAneosFile(IScheduler& scheduler, const Path& 
         throw InvalidSetup("Inconsistent number of temperatures in the ANEOS file '" + path.string() + "'");
     }
 
-    Lut2D<TRhoValue> ilut(n_T, n_rho, Ts.clone(), rhos.clone());
+    Lut2D<EosTabValue> lut(n_T, n_rho, std::move(Ts), std::move(rhos));
 
     // read the rho-T table
     std::stringstream ss;
-    Interval u_range;
     for (Size i_T = 0; i_T < n_T; ++i_T) {
         for (Size i_rho = 0; i_rho < n_rho; ++i_rho) {
             if (!std::getline(ifs, line)) {
@@ -104,34 +88,60 @@ Lut2D<Aneos::TabValue> Aneos::parseAneosFile(IScheduler& scheduler, const Path& 
             ss >> u >> P >> cs;
             SPH_ASSERT(ss);
 
-            ilut.at(i_T, i_rho) = TRhoValue{ u, P, 1.e-3_f * cs }; // km/s -> m/s
-            u_range.extend(u);
+            lut.at(i_T, i_rho) = EosTabValue{ u, P, 1.e-3_f * cs }; // km/s -> m/s
         }
     }
+    return lut;
+}
+
+Float getInitialDensity(const Lut2D<EosTabValue>& lut) {
+    const Float T_ref = 200._f; // K
+    const Float P_min = 1._f;   // Pa
+    for (Size i = 0; i < lut.getValuesY().size() - 1; ++i) {
+        const Float rho1 = lut.getValuesY()[i + 1];
+        const Float P1 = lut.interpolate(T_ref, rho1).P;
+        if (P1 > P_min) {
+            return lut.getValuesY()[i];
+        }
+    }
+    SPH_ASSERT(false); // no density in the range creates positive pressure?
+    return lut.getValuesY()[lut.getValuesY().size() / 2];
+}
+
+static Lut2D<Aneos::TabValue> transposeLut(IScheduler& scheduler, const Lut2D<EosTabValue>& ilut) {
+    const Array<Float>& Ts = ilut.getValuesX();
+    const Array<Float>& rhos = ilut.getValuesY();
+    const Size n_T = Ts.size();
+    const Size n_rho = rhos.size();
 
     //  generate u values to tabulate
     Array<Float> us(n_T);
     for (Size i_T = 0; i_T < n_T; ++i_T) {
         us[i_T] = ilut.at(i_T, n_rho / 2).u;
     }
+
     // make sure the range contains all values
+    Interval u_range;
+    for (const EosTabValue& value : ilut.data()) {
+        u_range.extend(value.u);
+    }
     us.front() = u_range.lower();
     us.back() = u_range.upper();
 
     // transpose from (T, rho)->(u,P,cs) to (rho,u)->(T,P,cs)
-    Lut2D<TabValue> lut(n_rho, n_T, rhos.clone(), us.clone());
+    Lut2D<Aneos::TabValue> lut(n_rho, n_T, rhos.clone(), us.clone());
     parallelFor(scheduler, 0, n_rho, 1, [&](const Size i_rho) {
         for (Size i_u = 0; i_u < us.size(); ++i_u) {
             // find the corresponding energy in the LUT
             const Float u = us[i_u];
-            ArrayView<const TRhoValue> row = ilut.row(i_rho);
+            ArrayView<const EosTabValue> row = ilut.row(i_rho);
             const auto iter = std::upper_bound(
-                row.begin(), row.end(), u, [](const Float u, const TRhoValue& iv) { return u < iv.u; });
+                row.begin(), row.end(), u, [](const Float u, const EosTabValue& iv) { return u < iv.u; });
             const Size i_T2 = Size(iter - row.begin());
             if (i_T2 > 0 && i_T2 < us.size()) {
                 const Size i_T1 = i_T2 - 1;
-                const TRhoValue iv1 = row[i_T1];
-                const TRhoValue iv2 = row[i_T2];
+                const EosTabValue iv1 = row[i_T1];
+                const EosTabValue iv2 = row[i_T2];
                 const Float u1 = iv1.u;
                 const Float u2 = iv2.u;
                 SPH_ASSERT(u1 <= u && u <= u2, u1, u, u2);
@@ -140,27 +150,30 @@ Lut2D<Aneos::TabValue> Aneos::parseAneosFile(IScheduler& scheduler, const Path& 
                 const Float P = lerp(iv1.P, iv2.P, f);
                 const Float cs = lerp(iv1.cs, iv2.cs, f);
                 const Float T = lerp(Ts[i_T1], Ts[i_T2], f);
-                lut.at(i_rho, i_u) = TabValue{ P, cs, T };
+                lut.at(i_rho, i_u) = Aneos::TabValue{ P, cs, T };
             } else if (i_T2 == 0) {
-                const TRhoValue iv = row.front();
-                lut.at(i_rho, i_u) = TabValue{ iv.P, iv.cs, Ts.front() };
+                const EosTabValue iv = row.front();
+                lut.at(i_rho, i_u) = Aneos::TabValue{ iv.P, iv.cs, Ts.front() };
             } else {
                 SPH_ASSERT(i_T2 == us.size());
-                const TRhoValue iv = row.back();
-                lut.at(i_rho, i_u) = TabValue{ iv.P, iv.cs, Ts.back() };
+                const EosTabValue iv = row.back();
+                lut.at(i_rho, i_u) = Aneos::TabValue{ iv.P, iv.cs, Ts.back() };
             }
         }
     });
     return lut;
 }
 
-Aneos::Aneos(const BodySettings& settings) {
-    const Path path(settings.get<String>(BodySettingsId::ANEOS_FILE));
-    if (path.empty()) {
-        throw InvalidSetup("No ANEOS file specified");
-    }
+
+//-----------------------------------------------------------------------------------------------------------
+// Aneos implementation
+//-----------------------------------------------------------------------------------------------------------
+
+
+Aneos::Aneos(const Path& path) {
+    Lut2D<EosTabValue> ilut = parseAneosFile(path);
     SharedPtr<IScheduler> scheduler = Factory::getScheduler();
-    lut = parseAneosFile(*scheduler, path);
+    lut = transposeLut(*scheduler, ilut);
 }
 
 Pair<Float> Aneos::evaluate(const Float rho, const Float u) const {
--- core/physics/Aneos.h
+++ core/physics/Aneos.h
@@ -7,9 +7,31 @@ NAMESPACE_SPH_BEGIN
 
 class Path;
 
+/// \brief State quantities for given value of temperature and density.
+struct EosTabValue {
+    Float u;
+    Float P;
+    Float cs;
+
+    EosTabValue operator+(const EosTabValue& other) const {
+        return { u + other.u, P + other.P, cs + other.cs };
+    }
+
+    EosTabValue operator*(const Float f) const {
+        return { u * f, P * f, cs * f };
+    }
+};
+
+/// \brief Reads ANEOS material file and returns the contents as a look-up table, mapping a paif of values
+/// (temperature, density) to corresponding values (specific energy, pressure, sound speed).
+Lut2D<EosTabValue> parseAneosFile(const Path& path);
+
+/// \brief Finds the initial density of the material from given look-up table.
+Float getInitialDensity(const Lut2D<EosTabValue>& lut);
+
 /// \brief ANEOS equation defined by a look-up table.
 class Aneos : public IEos {
-private:
+public:
     struct TabValue {
         Float P;
         Float cs;
@@ -19,10 +41,11 @@ private:
         TabValue operator*(const Float f) const;
     };
 
+private:
     Lut2D<TabValue> lut;
 
 public:
-    explicit Aneos(const BodySettings& settings);
+    explicit Aneos(const Path& path);
 
     virtual Pair<Float> evaluate(const Float rho, const Float u) const override;
 
@@ -37,9 +60,6 @@ public:
     virtual Float getDensity(const Float UNUSED(p), const Float UNUSED(u)) const override {
         NOT_IMPLEMENTED;
     }
-
-private:
-    static Lut2D<TabValue> parseAneosFile(IScheduler& scheduler, const Path& path);
 };
 
 NAMESPACE_SPH_END
